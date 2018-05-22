4da40bf9cd83e910c78b48f907b8ed869622da79
brunibrun <b.puri@gmx.de>
2018-05-22 17:29:06 +0200

    import of data
    first coding for importing of data

--- data/data_merger.R
+++ data/data_merger.R
@@ -18,10 +18,20 @@
 
 # Problem with working directory: Where?
 # setwd(??)
+# getwd()
 
-getwd()
 
-df.pun = read.csv("Elspot_Prices_Data.5375228caa4c48ad9b969f250d70fe2e", 
-               header = TRUE)
+# read in our data
+df.pun = read.csv("Elspot_Prices_Data-5375228caa4c48ad9b969f250d70fe2e.csv")
+df.solar = read.csv("Solarenergie_DE.csv")
+df.wind = read.csv("Windenergie_DE.csv")
+
+df.dem.2015 = read.csv("Total Load - Day Ahead _ Actual_201501010000-201601010000.csv")
+df.dem.2016 = read.csv("Total Load - Day Ahead _ Actual_201601010000-201701010000.csv")
+df.dem.2017 = read.csv("Total Load - Day Ahead _ Actual_201701010000-201801010000.csv")
+df.dem.2018 = read.csv("Total Load - Day Ahead _ Actual_201801010000-201901010000.csv")
+
+
+# now cleaning of unnecessary variables
 
 
