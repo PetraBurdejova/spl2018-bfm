801e32652325f6b6eb919f00f253a018ab11a53d
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2018-05-22 16:34:19 +0200

    Created new data subdirectory.

---
+++ data/dataMerger.R
@@ -0,0 +1,18 @@
+# ------------------------------
+# ------- dataMerger.R ---------
+# ------------------------------
+# 
+# Authors:  Bruno Puri, 
+#           Felix Germaine,
+#           Manuel Pfeuffer
+# 
+# Data from multiple .csv files is formated coherently and merged in a single
+# dataframe.
+#
+# Input: .csv files in the data/sources directory
+
+# Ouput: energy.csv
+#
+# ------------------------------
+
+
