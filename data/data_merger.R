289bd4f957989f865fccf4f473f1deb1bc274684
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2018-05-22 18:04:39 +0200

    Clean PUN data

--- data/data_merger.R
+++ data/data_merger.R
@@ -15,23 +15,24 @@
 #
 # ------------------------------
 
-
-# Problem with working directory: Where?
-# setwd(??)
-# getwd()
-
+library(readr)
 
 # read in our data
-df.pun = read.csv("Elspot_Prices_Data-5375228caa4c48ad9b969f250d70fe2e.csv")
-df.solar = read.csv("Solarenergie_DE.csv")
-df.wind = read.csv("Windenergie_DE.csv")
+df.pun    = read_csv2("source/Elspot_Prices_Data-5375228caa4c48ad9b969f250d70fe2e.csv")
+df.solar  = read_csv2("source/Solarenergie_DE.csv")
+df.wind   = read_csv2("source/Windenergie_DE.csv")
 
-df.dem.2015 = read.csv("Total Load - Day Ahead _ Actual_201501010000-201601010000.csv")
-df.dem.2016 = read.csv("Total Load - Day Ahead _ Actual_201601010000-201701010000.csv")
-df.dem.2017 = read.csv("Total Load - Day Ahead _ Actual_201701010000-201801010000.csv")
-df.dem.2018 = read.csv("Total Load - Day Ahead _ Actual_201801010000-201901010000.csv")
-
+df.dem.2015 = read_csv2("source/Total Load - Day Ahead _ Actual_201501010000-201601010000.csv")
+df.dem.2016 = read_csv2("source/Total Load - Day Ahead _ Actual_201601010000-201701010000.csv")
+df.dem.2017 = read_csv2("source/Total Load - Day Ahead _ Actual_201701010000-201801010000.csv")
+df.dem.2018 = read_csv2("source/Total Load - Day Ahead _ Actual_201801010000-201901010000.csv")
 
 # now cleaning of unnecessary variables
 
+# ---- PUN ----
+df.pun <- subset( df.pun, select = c(HourUTC, SpotPriceEUR ) )
+colnames(df.pun) = c("TIME", "PUN")
 
+# ---- SOLAR ----
+#df.solar <- subset( df.solar, select = c(Datum, von, bis, SpotPriceEUR ) )
+#colnames(df.pun) = c("TIME", "PUN")
