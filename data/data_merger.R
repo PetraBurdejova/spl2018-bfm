1dba01b43c50f21c15bb185c1105fba906743566
brunibrun <b.puri@gmx.de>
2018-05-23 07:17:30 +0200

    some csv2 back to csv
    nicht alle waren semikolon getrennte wert, deshalb nochmal zurück geändert..

--- data/data_merger.R
+++ data/data_merger.R
@@ -18,14 +18,14 @@
 library(readr)
 
 # read in our data
-df.pun    = read_csv2("source/Elspot_Prices_Data-5375228caa4c48ad9b969f250d70fe2e.csv")
+df.pun    = read_csv("source/Elspot_Prices_Data-5375228caa4c48ad9b969f250d70fe2e.csv")
 df.solar  = read_csv2("source/Solarenergie_DE.csv")
 df.wind   = read_csv2("source/Windenergie_DE.csv")
 
-df.dem.2015 = read_csv2("source/Total Load - Day Ahead _ Actual_201501010000-201601010000.csv")
-df.dem.2016 = read_csv2("source/Total Load - Day Ahead _ Actual_201601010000-201701010000.csv")
-df.dem.2017 = read_csv2("source/Total Load - Day Ahead _ Actual_201701010000-201801010000.csv")
-df.dem.2018 = read_csv2("source/Total Load - Day Ahead _ Actual_201801010000-201901010000.csv")
+df.dem.2015 = read_csv("source/Total Load - Day Ahead _ Actual_201501010000-201601010000.csv")
+df.dem.2016 = read_csv("source/Total Load - Day Ahead _ Actual_201601010000-201701010000.csv")
+df.dem.2017 = read_csv("source/Total Load - Day Ahead _ Actual_201701010000-201801010000.csv")
+df.dem.2018 = read_csv("source/Total Load - Day Ahead _ Actual_201801010000-201901010000.csv")
 
 # now cleaning of unnecessary variables
 
@@ -36,3 +36,4 @@ colnames(df.pun) = c("TIME", "PUN")
 # ---- SOLAR ----
 #df.solar <- subset( df.solar, select = c(Datum, von, bis, SpotPriceEUR ) )
 #colnames(df.pun) = c("TIME", "PUN")
+
