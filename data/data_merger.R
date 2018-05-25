61e3a9bb715a7a2aed825577bccac07d9709fadf
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2018-05-25 12:46:24 +0200

    Tweak comment aesthetics

--- data/data_merger.R
+++ data/data_merger.R
@@ -9,15 +9,24 @@
 # Data from multiple .csv files is formated coherently and merged in a single
 # dataframe.
 #
-# Input: .csv files in the data/sources directory
+# Input:  .csv files in the data/sources directory
 
-# Ouput: energy.csv
+# Ouput:  A dataframe containing the variables
+#             TIME
+#             PUN
+#             SOLAR
+#             WIND
+#             DEM
+#             (GAS)
 #
 # ------------------------------
 
+
 library(readr)
 
-# read in our data
+
+# 1. READ DATA FROM DATA/SOURCE SUBDIRECTORY
+
 df.pun    = read_csv("source/Elspot_Prices_Data-5375228caa4c48ad9b969f250d70fe2e.csv")
 df.solar  = read_csv2("source/Solarenergie_DE.csv")
 df.wind   = read_csv2("source/Windenergie_DE.csv")
@@ -27,13 +36,20 @@ df.dem.2016 = read_csv("source/Total Load - Day Ahead _ Actual_201601010000-2017
 df.dem.2017 = read_csv("source/Total Load - Day Ahead _ Actual_201701010000-201801010000.csv")
 df.dem.2018 = read_csv("source/Total Load - Day Ahead _ Actual_201801010000-201901010000.csv")
 
-# now cleaning of unnecessary variables
 
-# ---- PUN ----
+# 2. PREPARE DATA FOR MERGE
+
+# 2a. PUN
 df.pun <- subset( df.pun, select = c(HourUTC, SpotPriceEUR ) )
 colnames(df.pun) = c("TIME", "PUN")
 
-# ---- SOLAR ----
+# 2b. SOLAR
 #df.solar <- subset( df.solar, select = c(Datum, von, bis, SpotPriceEUR ) )
 #colnames(df.pun) = c("TIME", "PUN")
+
+# 2c. WIND
+
+# 2d. DEM
+
+# (2e. GAS)
 
