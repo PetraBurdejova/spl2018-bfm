3a7abde7c219042b9da8187f856652c81f2ca24c
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2018-06-05 18:33:59 +0200

    Change minor things to adhere to Google Styleguide
    Renamed file and changed function name.

--- data/clean_missing_values.R
+++ data/clean_missing_values.R
@@ -1,8 +1,5 @@
-### --------- ###
-# - FUNCTIONS - #
-# ------------- #
 
-findMissingValues <- function(df, verbose = FALSE, days = FALSE) {
+FindMissingValues <- function(df, verbose = FALSE, days = FALSE) {
   # Checks for NA values in dataframe and prints information. Returns a list of
   # indices of the NA entries in dataframe.
   #
@@ -21,12 +18,12 @@ findMissingValues <- function(df, verbose = FALSE, days = FALSE) {
   if (verbose == TRUE) {
 
     name <- deparse(substitute(df))  # get name of dataframe
-    no_na <- length(indices)  # number of na in df
+    no.na <- length(indices)  # number of na in df
     entries <- nrow(df)  # number of values in df
 
     print(sprintf("Checking for missing values in dataframe '%s'.", name))
     print(sprintf("%.0f of %.0f (%.3f%%) values are NA.",
-                  no_na, entries, no_na/entries))
+                  no.na, entries, no.na/entries))
 
   }
 
