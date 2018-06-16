285f1a90b14b60f9610d537dc477df11528656f2
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2018-06-16 21:58:39 +0200

    Add verbose flag

--- data/clean_missing_values.R
+++ data/clean_missing_values.R
@@ -1,14 +1,10 @@
 
-DiagMissingValues <- function(df, verbose = FALSE, days = FALSE) {
-  # Checks for NA values in dataframe and prints information. Returns a list of
-  # indices of the NA entries in dataframe.
+DiagMissingValues <- function(df, verbose = FALSE) {
+  # Checks for NA values in dataframe and prints information.
   #
   # Args:
   #   df: The dataframe that will be checked for missing values.
-  #   verbose: If TRUE, prints information on the missing values such as num-
-  #            ber and percentage of missing values, as well as number of days
-  #            affected.
-  #   days: If TRUE, returns a list of days with at least one missing value.
+  #   verbose: Print more detailed information.
   #
   # Returns:
   #   A list of indices that correspond to the possiton of missing values in df.
@@ -17,24 +13,26 @@ DiagMissingValues <- function(df, verbose = FALSE, days = FALSE) {
 
   r <- is.na(df)
   cc <- complete.cases(df)
-  
+
   # Diagnostics Output
-  cat(sprintf("\n---- NA Diagnostics for '%s' ----\n", name))
+  cat(sprintf("---- NA Diagnostics for '%s' ----\n", name))
   cat(sprintf("Number of complete cases: %i of %i.\n", 
               sum(cc), nrow(df) ))
-  cat(sprintf("Number of incomplete cases: %i (%.3f%%).\n\n\n",
-              (nrow(df)-sum(cc)), (1-sum(cc)/nrow(df)) ))
-  cat(sprintf("NA's per column:\n\n"))
-  print(apply(r, 2, sum))
-  cat("\n")
-  
-  i <- 1
-  for (col in names(df)){
-    cat(sprintf("NA's in at least %i columns: %i\n",
-                i, sum(apply(r, 1, sum) >= i) ))
-    i <- i+1
-  }
+  cat(sprintf("Number of incomplete cases: %i (%.3f%%).\n",
+              (nrow(df)-sum(cc)), (1-sum(cc)/nrow(df))*100) )
 
-  # TODO: Add regression tests.
+  if (verbose == TRUE) {
+    cat(sprintf("NA's per column:\n\n"))
+    print(apply(r, 2, sum))
+    cat("\n")
+
+    i <- 1
+    for (col in names(df)){
+      cat(sprintf("NA's in at least %i columns: %i\n",
+                  i, sum(apply(r, 1, sum) >= i) ))
+      i <- i+1
+    }
+
+  }
 
 }
