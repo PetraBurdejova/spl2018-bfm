f055e6f61fb0355d97cd9fb8e8ad4a2a72d777b4
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2018-06-18 14:06:23 +0200

    DiagMissingValues() now returns timepoints
    Also changed the diagnostics handling to use dlevel flags.

--- data/clean_missing_values.R
+++ data/clean_missing_values.R
@@ -1,31 +1,32 @@
 
-DiagMissingValues <- function(df, verbose = FALSE) {
+DiagMissingValues <- function(df, dlevel = 0) {
   # Checks for NA values in dataframe and prints information.
   #
   # Args:
   #   df: The dataframe that will be checked for missing values.
-  #   verbose: Print more detailed information.
+  #   dlevel: Print more detailed information. 0: No information.
+  #           1: General information. 2: Information on columns.
   #
   # Returns:
-  #   A list of indices that correspond to the possiton of missing values in df.
+  #   Returns a list of timepoints of incomplete cases in df.
 
   name <- deparse(substitute(df))  # get name of dataframe
 
   r <- is.na(df)
   cc <- complete.cases(df)
 
-  # Diagnostics Output
-  cat(sprintf("---- NA Diagnostics for '%s' ----\n", name))
-  cat(sprintf("Number of complete cases: %i of %i.\n", 
-              sum(cc), nrow(df) ))
-  cat(sprintf("Number of incomplete cases: %i (%.3f%%).\n",
-              (nrow(df)-sum(cc)), (1-sum(cc)/nrow(df))*100) )
+  if (dlevel >= 1) {
+    # dlevel 1 diagnostics: general information.
+    cat(sprintf("------ NA Diagnostics for '%s' ------\n", name))
+    cat(sprintf("Number of complete cases: %i of %i.\n", 
+                sum(cc), nrow(df) ))
+    cat(sprintf("Number of incomplete cases: %i (%.3f%%).\n",
+                (nrow(df)-sum(cc)), (1-sum(cc)/nrow(df))*100) )
+  }
 
-  if (verbose == TRUE) {
-    cat(sprintf("NA's per column:\n\n"))
+  if (dlevel >= 2) {
+    # dlevel 2 diagnostics: information per column.
     print(apply(r, 2, sum))
-    cat("\n")
-
     i <- 1
     for (col in names(df)){
       cat(sprintf("NA's in at least %i columns: %i\n",
@@ -35,4 +36,8 @@ DiagMissingValues <- function(df, verbose = FALSE) {
 
   }
 
+  tp <- subset(df$TIME, complete.cases(df) == FALSE)
+  # Get incomplete timepoints.
+
+  return(tp)
 }
