5fcba58115fe28cef7690c27e03447931991e252
brunibrun <b.puri@gmx.de>
2018-06-19 17:45:26 +0200

    add "plot ideas" in graphics

--- graphics/graphics_first.try.R
+++ graphics/graphics_first.try.R
@@ -25,3 +25,67 @@ ggplot(data=df, aes(x=`TIME`, y=log(`SOLAR.MW/h`))) +
 
 
 
+### ideen:
+
+
+library(ggplot2)
+
+## SUN DE --
+
+ggplot(data=df, aes(x=`TIME`, y=log(`SOLAR`))) +  
+  geom_point(size=0.5) + 
+  # geom_smooth(method="lm", aes(fill=`TIME`)) + 
+  facet_wrap(~year(TIME)) +
+  geom_smooth(method="loess", color="blue", se=FALSE)
+# nur ein jahranzeigen pro grafik
+
+
+## WIND --
+
+ggplot(data=df, aes(x=`TIME`, y=log(`WIND`))) +  
+  geom_point(size=0.5) + 
+  geom_smooth(method="lm", aes(fill=`TIME`))  
+# facet_wrap(~year(TIME)) +
+# geom_smooth(method="loess", color="blue", se=FALSE)
+
+
+## PUN --
+
+ggplot(data=df, aes(x=`TIME`, y=`PUN`))  +
+  geom_point(size=0.5, color = "black") + 
+  # geom_smooth(method="lm", aes(fill=`TIME`), color = "black")  + 
+  geom_smooth(method="loess", color="blue", se=FALSE) 
+
+
+## DEMAND -- 
+
+ggplot(data=df, aes(x=`TIME`, y=log(`DEM`))) +  
+  geom_point(size=0.5) + 
+  # geom_smooth(method="lm", aes(fill=`TIME`)) + 
+  # facet_wrap(~year(TIME)) +
+  geom_smooth(method="loess", color="green", se=FALSE) +
+  ylim(15.5, 15.8)
+# facet_wrap(~year(TIME)) 
+
+
+## PUN ~ SOLAR --   
+
+ggplot(data=df, aes(y=`PUN`, x=log(`SOLAR`))) +  
+  ylim(200,1500) +
+  geom_point(size=0.5) + 
+  geom_smooth(method="lm", aes(fill=`TIME`))  
+# facet_wrap(~year(TIME)) +
+# geom_smooth(method="loess", color="green", se=FALSE) +
+# facet_wrap(~year(TIME)) 
+
+
+## PUN ~ DEMAND --
+
+ggplot(data=df, aes(y=`PUN`, x=log(`DEM`))) +  
+  ylim(0,1500) +
+  geom_point(size=0.5) + 
+  geom_smooth(method="lm", aes(fill=`TIME`))  
+# facet_wrap(~year(TIME)) +
+# geom_smooth(method="loess", color="green", se=FALSE) 
+# facet_wrap(~year(TIME)) 
+
