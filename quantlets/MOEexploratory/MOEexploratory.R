0959d477ae0e53ad5962a5366151ce81e2a04b1b
brunibrun <b.puri@gmx.de>
2018-07-06 12:52:14 +0200

    formating exploratory

--- quantlets/MOEexploratory/MOEexploratory.R
+++ quantlets/MOEexploratory/MOEexploratory.R
@@ -1,27 +1,71 @@
-##### SPL Data visualization ------
-# My first try at graphics.
-# just ideas. I am going to do the ggplot 2 courses soon.
-# if you have improvements/ other ideas go wild ;)
+###############################################################################     
+####    MOEexploratory.R    #####################################################     
+###############################################################################     
+#
+# Loads data set from 'MOEmergedata'. Creates exploratory plots of data 
+#
+# Input:  '.Rdata' file from the 'MOEmergedata' Quantlet.
+#
+# Ouput:  MOEdata_merge.csv     - data in table form
+#         MOEdata_merge.Rdata   - data in Rdata form
+#
+###############################################################################
 
-## LOADING DATA FROM DATA-MERGER & LOADING PACKAGES -----
+# Clear all variables.
+rm(list = ls(all = TRUE))
+graphics.off()
 
-source("data_merger.R")
-library(ggplot2)
+# Install and load libraries.
+libraries = c("ggplot2", "tikzDevice")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+
+
+###############################################################################
+####    0.  SET WORKING DIRECTORY    ##########################################
+###############################################################################
+####    ATTENTION: Working directory is assumed to be the root of the MOE 
+####    repository, not the MOEmergedata Quantlet subdirectory!!!
+
+
+# If needed, set working directory accordingly:
+#setwd("path/to/MOE_repository")
+
+
+
+###############################################################################
+####    1.  LOAD DATA    ######################################################
+###############################################################################
+
+
+# Grab data from 'MOEmergedata' Quantlet
+load("MOEmergedata/MOEdata_merge.Rdata")
+
 
 ## GRAPHICS -----
 ## SUN --
 
 # Sun per Year 
-ggplot(data=df, aes(x=TIME, y=SOLAR)) +  
-  geom_point(size=0.5) + 
-  geom_smooth(method="loess", span = 0.25, color="orange", se=FALSE) + 
-  #ggtitle(label = "Solar Energy Production per Day", 
-   #       subtitle = "German and Austrian Production in MW/h") +
-  xlab(label= " ") +
-  ylab(label = "MW/h")+
-  theme_bw()
-  # labs(caption = "(based on data from 'Netztransparenz' and 'APG' ")
+options(tz="Berlin")
 
+tikz(file = "plot1.tex", width = 5, height = 5, console = T, engine = getOption("tikzDefaultEngine"))
+
+plot = ggplot(data=df, aes(x=TIME, y=SOLAR)) +  
+          geom_point(size=0.5) + 
+          geom_smooth(method="loess", span = 0.25, color="orange", se=FALSE) + 
+          ggtitle(label = "Solar Energy Production per Day", 
+                  subtitle = "German and Austrian Production in MW/h") +
+          xlab(label= " ") +
+          ylab(label = "MW/h")+
+          labs(caption = "(based on data from 'Netztransparenz' and 'APG' ")
+        
+print(plot)
+dev.off()
+
+options(tikzLatex = )
+?tikz
 
 ggsave("Solar Production", plot = last_plot(), device = "pdf", 
        path =  NULL, scale = 1.5, width = 7.5 , height = 5, 
