0d723d56eafd49ef7181ae003db7f77b78258b82
felixGer <felixgermaine1@gmail.com>
2018-08-08 13:48:29 +0200

    Clean Regression with output in latex format

--- quantlets/MOEregression/MOEregression.R
+++ quantlets/MOEregression/MOEregression.R
@@ -1,25 +1,48 @@
+# The clean daily energy data ( without NA's) is loaded in order to perform
+# an OLS regression and the prais winsten regression.
+# The following tests are performedalong the way: 
+# -Augmented Dickey fuller test and philipps perron test for stationarity of 
+# the energy data ( with trend and constant)
+# -Durbin watson test for autocorrelation in the OLS
+# -Breusch-Pagan for heteroscedastiocity in the OLS
+# -Durbin watson test for autocorrelation in the Prais winsten regression
+#  -The ACF and PACF are also plotted for both OLS and Prais winsten
+#
+# Input:  MOEmergedata.R 
+#
+# Ouput: - ACF and PACF plot of the OLS and Prais Winsten (2pdf files)
+#        - A latex table for the ADF and PP tests for stationnarity
+#        - A latex table for the Durbin Watson and 
+#          Breusch Pagan tests on the OLS
+#        - A latex table for the coefficients of the Prais Winsten regression
+#        - A latex table for R^2, Adj. R^2 and Durbin Watson test results of 
+#          the prais winsten regression
+###############################################################################
+
+
+# Clear all variables.
+rm(list = ls(all = TRUE))
+graphics.off()
+
+# Install and load libraries.
+libraries = c("xts", "prais","lmtest", "forecast", "astsa", "tseries", "xtable")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+
+#library("xts") # xts
+#library("prais") # prais winsten regression
+#library("lmtest") #dwtest and #
+#library("forecast") #tslm
+#library("astsa") # acf2
+#library("tseries") #adf.test
+#library("xtable")
+
+
 
 ###############################################################################
-####  ####################################
-###############################################################################
-
-
-library("astsa") # acf2
-library("xts") # xts
-
-library("prais") # prais winsten regression
-library("lmtest") #dwtest and #
-library("forecast") #tslm
-
-library("aTSA") # for pptest
-library("tseries") #adf.test
-library("xtable")
-# library("dyn") ##?
-# library("nlme") #??
-
-
-###############################################################################
-####   Loading and preparing the data #########################################
+####   0. Loading and preparing the data ######################################
 ###############################################################################
 #setwd("C:/Users/PC-FELIX/Documents/GitHub/spl2018-bfm/quantlets")
 
@@ -38,12 +61,16 @@ source("MOEtimedummies/MOEtimedummies.R")
 
 xts.mydata = YMDDummy(xts.mydata)
 # adds the dummy matrix for Year, Month and day of the week to the data
-ts.mydata = as.ts(xts.mydata)
-ts.mydata[,2:4] = as.ts(xts.mydata)[,2:4]/24000
-## changes the scale of the data to facilitate the interpretation
-## generation data is now expressed in mean hourly generation in GWh(thus GW)
-## Price is still in EUR/MWh 
 
+
+ts.mydata = as.ts(xts.mydata)
+# transforms the data into the ts format in order to perform the regression
+
+
+ts.mydata[,2:4] = as.ts(xts.mydata)[,2:4]/24000
+# changes the scale of the data to facilitate the interpretation
+# generation data is now expressed in mean hourly generation in GWh(thus GW)
+# Price is still in EUR/MWh 
 # convert to ts format for regression
 
 
@@ -55,30 +82,29 @@ ts.mydata[,2:4] = as.ts(xts.mydata)[,2:4]/24000
 Results.ADF = rep(NA, 4)
 
 for (Column in 1:4) {
-  Results.ADF[Column]=tseries::adf.test(ts.mydata[, Column], alternative = "stationary")$p.value
+  Results.ADF[Column]=tseries::adf.test(ts.mydata[, Column],
+                                        alternative = "stationary")$p.value
 }
+# Performs the ADF test for the variables Price, Demand, Solar and Wind
+# the p-values are stored in Results.ADF
+# H0: non-stationary ( with time trend and constant)
 
 Results.PP = rep(NA, 4)
 
 for (Column in 1:4) {
   Results.PP[Column]= tseries::pp.test(ts.mydata[, Column])$p.value
 }
+# Performs the PP test for the variables Price, Demand, Solar and Wind
+# the p-values are stored in Results.PP
+# H0: non-stationary ( with time trend and constant)
+
 
 Table1 = data.frame(Results.ADF, Results.PP)
 colnames(Table1) = c("A. Dickey Fuller", "Philipps-perron")
 rownames(Table1) = c("El. Price","El. Demand", "Solar Gen.", "Wind Gen.")
-
-xtable(Table1)
-
-
-
-## Trending 
-# detr.PUN <- tslm(PUN ~ trend, ts.mydata)
-# detr.DEM <- tslm(DEM ~ trend, ts.mydata)
-# detr.SOLAR <- tslm(SOLAR ~ trend, ts.mydata)
-# detr.WIND <- tslm(WIND ~ trend, ts.mydata)
-
-
+TABLE1 = xtable(Table1)
+print.xtable(TABLE1, type="latex", file="MOEregression/ADFandPPTEST")
+# Arranges the p-values of the ADF test and PP Test into a Latex table
 
 
 ###############################################################################
@@ -87,9 +113,8 @@ xtable(Table1)
 ###############################################################################
 
 
-OLS = tslm(PUN ~ trend + ts.mydata[,-1], ts.mydata)
-##summary(OLS)$r.squared 
-##summary(OLS)$adj.r.squared 
+OLS = tslm(PUN ~  ts.mydata[,-1], ts.mydata)
+# Performs the basic OLS 
 
 DWTEST.OLS = dwtest(OLS)
 # perform the durbin watson test in order to check for autocorrelation of 
@@ -98,27 +123,20 @@ DWTEST.OLS = dwtest(OLS)
 BPTEST.OLS = bptest(OLS)
 # Performs the breusch pagan test for heteroscedasticity
 # Under H0 the test statistic of the Breusch-Pagan test follows a chi-squared
-# distribution with parameter (the number of regressors without the constant in 
-# the model) degrees of freedom
+# distribution degrees of freedom
 
 Table2 = data.frame(DWTEST.OLS$p.value,BPTEST.OLS$p.value)
 colnames(Table2) = c("Durbin-Watson", "Breusch-Pagan")
 row.names(Table2) = "p-value"
-xtable(Table2)
+TABLE2 = xtable(Table2)
+print.xtable(TABLE2, type="latex", file="MOEregression/OLS_DWandBPTEST")
+# Stores the p values of the DWtest and the BP test into a latex table
 
 
 jpeg('MOEregression/PACF_OLS.jpg')
-acf2(OLS$residuals)
+acf2(OLS$residuals, main="OLS residuals")
 dev.off()
-# generates a jpeg file of a plot of the PACF and the ACF
-
-##AR and MA signatures: If the PACF displays a sharp cutoff while the ACF decays more slowly 
-##(i.e., has significant spikes at higher lags), we say that the stationarized series displays an "AR signature," 
-##meaning that the autocorrelation pattern can be explained more easily by adding AR terms than by adding MA terms
-
-
-
-
+# generates a jpeg file of a plot of the PACF and the ACF of the OLS
 
 
 ###############################################################################
@@ -133,40 +151,68 @@ PWReg = prais.winsten(PUN ~ .,ts.mydata,iter = 50,rho = 0, tol = 1e-08)
 DWTEST.PW = dwtest(PWReg[[1]])
 # Durbin Watson Test for autocorrelation after correcting for serial correlation
 
+
+###############################################################################
+# 3.2 Generate latex and jpeg output
+###############################################################################
+
+# 3.2.1 ACF and PACF plot
+
 jpeg('MOEregression/PACF_PraisWinsten.jpg')
-acf2(PWReg[[1]]$residuals)
+acf2(PWReg[[1]]$residuals, main="Prais-Winsten residuals")
 dev.off()
 # generates a jpeg file of the plots of the ACF and the PACF
 
-
+#3.2.2 Latex for Prais winsten regression
 
 coef = PWReg[[1]][, drop=F]$coefficients
 coef = rbind(coef[-1,], coef[1,])
 
 rho = PWReg[[2]]
 rho = as.numeric(c(PWReg[[2]][1,1], NA ,PWReg[[2]][1,2], NA))
-# calculate p-statistic?
 
 Table3 = rbind(coef, rho)
 Table3 = as.data.frame(Table3)
 rownames(Table3) = c("Demand", "Solar Gen.", "Wind Gen.",
-                     "Year 2016", "Year 2017", "February", "March", "April", "May", "June", "July", 
-                     "August", "September", "October", "November", "December", "Monday", "Tuesday",
-                     "Wednesday", "Thursday","Friday", "Saturday", "Intercept",
+                     "Year 2016", "Year 2017", "February",
+                     "March", "April", "May", "June", "July", 
+                     "August", "September", "October", "November",
+                     "December", "Monday", "Tuesday", "Wednesday",
+                     "Thursday","Friday", "Saturday", "Intercept",
                      "\rho  (AR1)")
 
-xtable(Table3, caption = "Prais-Winsten regression results", align =c("l", "r","r", "r","r"), digits=2)
+TABLE3 = xtable(Table3,
+                caption = "Prais-Winsten regression results",
+                align =c("l", "r","r", "r","r"), 
+                digits=2)
 
-
+print.xtable(TABLE3, type ="latex",
+             file = "MOEregression/PraisWinstenRegCoefficients")
+#generates a Latex table yith the coefficients of the Prais-Winsten regression
 
 
 Table4 = data.frame(PWReg[[1]]$r.squared, 
            PWReg[[1]]$adj.r.squared, 
            DWTEST.PW$p.value)
-colnames(Table4) = c("R^2","Adj. R^2","Durbin-Watson(p-val)")
+colnames(Table4) = c("$R^2$","$Adj. R^2$","Durbin-Watson (p value )")
 rownames(Table4) = ""
-test = xtable(Table4, caption = "Prais-Winsten regression results", align=c("l","c", "c", "c"), digits=2)
-print.xtable(test, type="latex", file="test2")
+
+
+TABLE4 = xtable(Table4, 
+                caption = "Prais-Winsten regression results",
+                align=c("l","c", "c", "c"),
+                digits=2)
+
+print.xtable(TABLE4, 
+             type="latex",
+             file="MOEregression/PraisWinstenGoFDW", 
+             sanitize.text.function=function(x){x})
+# generates a Latex table with the R^2, the Adj R^2 and the Result of the durbin
+# watson test fopr the prais winsten regression
+
+
+rm(list = ls(all = TRUE))
+graphics.off()
 
 
 
